8592ebf4e64a7f5e4e5e8b352ac95f830230360a
bharathraj <97496230+Bharathrajskillpro@users.noreply.github.com>
2023-08-18 15:45:12 +0530

    project setup done

---
+++ server/models/User.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+const paymentSchema = new mongoose.Schema({
+  package: {
+    type: String,
+    required: true,
+    enum: ['learner', 'creator', 'pro'],
+    default: 'learner',
+  },
+  payterm: {
+    type: String,
+    required: true,
+    enum: ['week', 'month', 'year', 'free'],
+    default: 'free',
+  },
+  lastpay: {
+    type: Date,
+  },
+  history: {
+    type: Array,
+  }
+});
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    maxlength: 50,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  interests: {
+    type: [String],
+    default: [],
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  // paymentDetail: {
+  //   type: paymentSchema,
+  //   required: true
+  // }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
